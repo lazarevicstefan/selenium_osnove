67df1dd43df4aa979fd401ac25ab021a1c190bd9
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-09-27 15:33:53 +0200

    Added task description

--- src/main/java/d22_09_2023/Zadatak_02.java
+++ src/main/java/d22_09_2023/Zadatak_02.java
@@ -9,6 +9,20 @@ import java.util.List;
 
 public class Zadatak_02 {
     public static void main(String[] args) throws InterruptedException {
+        /*
+        Niz od 5 stringova. Svaki element u nizu je url stranice:
+        https://www.google.com/
+        https://www.facebook.com/
+        https://www.youtube.com/
+        https://www.ebay.com/
+        https://www.katalon.com/
+        zatim koristeci for petlju otvara svaku stranicu iz niza u pretrazivacu prateci sledeca pravila:
+        za svaku stranicu se kreira nova infrastuktura
+        ucitava stranica
+        pravi pauza od 1s
+        ponistava testna stuktura
+
+         */
         List<String> nizUrl = new ArrayList<>();
         nizUrl.add("https://www.google.com/");
         nizUrl.add("https://www.facebook.com/");
