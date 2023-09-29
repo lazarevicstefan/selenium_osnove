ce167167817c2bdc93b5a5372c4e0d15daf92a84
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-09-29 18:37:24 +0200

    Add Implicitly wait sample code

---
+++ src/main/java/p28_09_2023/ImplicitlyWait.java
@@ -0,0 +1,24 @@
+package p28_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ImplicitlyWait {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        webDriver.get("https://demoqa.com/dynamic-properties");
+
+        webDriver.findElement(By.id("visibleAfter")).click();
+
+        webDriver.quit();
+    }
+}
