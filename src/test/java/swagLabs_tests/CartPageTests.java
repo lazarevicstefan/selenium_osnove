8f3a21dd29b6c605bb101da4c496de6d9094c451
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-10-11 15:46:48 +0200

    Add Verify Title In Head test

--- src/test/java/swagLabs_tests/CartPageTests.java
+++ src/test/java/swagLabs_tests/CartPageTests.java
@@ -1,7 +1,11 @@
 package swagLabs_tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import swagLabs_retry.swagLabsRetry;
 
 public class CartPageTests extends BasicTest{
     @Test
@@ -13,10 +17,18 @@ public class CartPageTests extends BasicTest{
     public void verifyTheTitlePage (){
         String expectedTitle = "Swag Labs";
         String product = "Sauce Labs Backpack";
+
         inventoryPage.addToCartProduct(product);
         inventoryPage.clickOnCartIcon();
         Assert.assertEquals(cartPage.getTitle(),expectedTitle
                 ,"Title should be '" + expectedTitle + "'");
     }
+    @Test(priority = 3)
+    public void verifyTheTitleInHead(){
+        String expectedTitle = "Swag Labs";
 
+        inventoryPage.clickOnCartIcon();
+        Assert.assertEquals(cartPage.getTitleInHead(),expectedTitle
+                ,"Title should be '" + expectedTitle + "'");
+    }
 }
