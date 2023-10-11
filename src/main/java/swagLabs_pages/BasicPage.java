4038ca0ae56296465ad61177f8f6862715311dd0
Stefan Lazarevic <lazarevics1995@gmail.com>
2023-10-11 17:38:57 +0200

    Add Basic Abstract Page

---
+++ src/main/java/swagLabs_pages/BasicPage.java
@@ -0,0 +1,15 @@
+package swagLabs_pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasicPage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+
+    public BasicPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+}
